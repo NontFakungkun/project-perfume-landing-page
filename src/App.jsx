0e09ea8ebaa893b5bf7f7e1000c7ef2cd63fbbe0
Nont Fakungkun <nont.fkn@gmail.com>
2024-06-02 15:29:40 +0700

    fix overlap issue

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Nav from "./components/Nav";
 const App = () => (
   <main className="relative">
     <Nav/>
-    <section className="xl:padding-l wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b py-8">
       <Hero/>
     </section>
     <section className="padding">
@@ -26,10 +26,7 @@ const App = () => (
     {/* <section className="padding">
       <SpecialOffer/>
     </section> */}
-    <section className="padding bg-pale-blue">
-      <CustomerReviews/>
-    </section>
-    <section className="padding-x sm:py-32 py-16 w-full">
+    <section className="padding-x bg-pale-blue sm:py-32 py-16 w-full">
       <Subscribe/>
     </section>
     <section className="padding bg-black padding-x padding-t pb-8">
